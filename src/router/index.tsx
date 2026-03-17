3525ef5506f896e59072c09802397ea2ad1b9eaf
rkdeofuf <asa47972000@naver.com>
2026-03-17 23:23:53 +0900

    Feat: add privacy route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,15 @@
 import BasePage from '@/pages/base-page'
 import HomeScreen from '@/screens/home'
+import PrivacyScreen from '@/screens/privacy'
 import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <BasePage />,
-    children: [{ index: true, element: <HomeScreen /> }],
+    children: [
+      { index: true, element: <HomeScreen /> },
+      { path: 'privacy', element: <PrivacyScreen /> },
+    ],
   },
 ])
---
+++ src/screens/privacy/index.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import { Arrow } from './components/arrow'
+import './styles.scss'
+import ParkeLogo from '@/assets/parke-icon.png'
+import PrivacyModal from '@/screens/privacy/components/privacy-modal'
+import { type DocType, type AppName } from './types'
+// import { PrivacyScreen } from './components/PrivacyScreen'
+
+export default function Privacy() {
+  const [activeDoc, setActiveDoc] = useState(false)
+  const [appName, setAppName] = useState<AppName>(null)
+  const [docType, setDocType] = useState<DocType>(null)
+  const handleShowPress = (name: AppName, type: DocType) => {
+    setActiveDoc(true)
+    setAppName(name)
+    setDocType(type)
+  }
+
+  const handleClose = () => {
+    setActiveDoc(false)
+  }
+
+  return (
+    <div className="privacy-container">
+      {/* <PrivacyScreen /> */}
+      <div className="privacy-wrapper">
+        <div className="privacy-title">이용약관</div>
+        <div className="privacy-items">
+          <div className="privacy-item">
+            <div className="item-top">
+              <div className="item-box">
+                <img src={ParkeLogo} />
+              </div>
+              <div className="desc-box">
+                <div className="desc-title">Parke</div>
+                <div className="desc-subtitle">Smart Parking Solution</div>
+              </div>
+            </div>
+            <div className="privacy-box">
+              <div
+                className="privacy-term"
+                onClick={() => handleShowPress('parke', 'privacy')}
+              >
+                개인정보 처리방침 <Arrow size={16} />
+              </div>
+              <div className="divider" />
+              <div
+                className="privacy-term"
+                onClick={() => handleShowPress('parke', 'terms')}
+              >
+                서비스 이용약관 <Arrow size={16} />
+              </div>
+              <div className="divider" />
+              <div
+                className="privacy-term"
+                onClick={() => handleShowPress('parke', 'consent')}
+              >
+                개인정보 수집이용 동의서 <Arrow size={16} />
+              </div>
+              <div className="divider" />
+              <div
+                className="privacy-term"
+                onClick={() => handleShowPress('parke', 'consent-third')}
+              >
+                개인정보 제3자제공 동의서 <Arrow size={16} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {activeDoc && appName && docType && (
+        <PrivacyModal
+          appName={appName}
+          handleClose={handleClose}
+          docType={docType}
+        />
+      )}
+    </div>
+  )
+}
