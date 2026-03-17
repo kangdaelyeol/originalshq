92d7fbf77ab6d849d693ce9064b8e76b0e4eee11
rkdeofuf <asa47972000@naver.com>
2026-03-17 16:42:34 +0900

    Feat: add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { About } from './components/about'
-import { BackToTopButton } from './components/back-to-top-button'
-import { Contact } from './components/contact'
-import { Footer } from './components/footer'
-import { Hero } from './components/hero'
-import { Nav } from './components/navigation-bar'
-import { Products } from './components/products'
-import { RevealContextProvider } from './context/reveal-context'
+import { RevealContextProvider } from '@/context/reveal-context'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
 function App() {
   return (
     <RevealContextProvider>
-      <Nav />
-      <Hero />
-      <About />
-      <Products />
-      <Contact />
-      <Footer />
-      <BackToTopButton />
+      <RouterProvider router={router} />
     </RevealContextProvider>
   )
 }
