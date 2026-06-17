39abe6f5778186ad1f97dab1998fafcad21c744a
rkdeofuf <asa47972000@naver.com>
2026-06-17 13:11:13 +0900

    Feat: add Header scroll action

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
-import LogoIcon from '@/assets/originals_logo.svg'
 import LogoSymbol from '@/assets/logo_symbol.svg'
 import LogoCaption from '@/assets/logo_caption.svg'
 import LogoText from '@/assets/logo_text.svg'
@@ -18,13 +17,17 @@ const navMenuList = [
 export const Header = () => {
   const { menuRef, isMenuOpen, scrolled, setIsMenuOpen, hamburgerRef } =
     useHeader()
-    
+
   return (
     <section className={['header', 'navbar', scrolled && 'scrolled'].join(' ')}>
       <div className="container">
         <div className="wrapper">
-          <a className="logo-container" href="/#home">
-            <img src={LogoIcon} alt="logo icon" />
+          <a className="logo__container" href="/#home">
+            <img src={LogoSymbol} className="logo-symbol" alt="logo icon" />
+            <div className="text__container">
+              <img src={LogoCaption} className="logo-caption" alt="logotext" />
+              <img src={LogoText} className="logo-text" alt="logotext" />
+            </div>
           </a>
 
           <div
