830558b6ce02b20b7442447f1d9ce7e69be1cfb2
rkdeofuf <asa47972000@naver.com>
2026-07-22 12:36:51 +0900

    Feat: add search icon svg

---
+++ src/screens/xtool-lead-manager/illustration/search-icon.tsx
@@ -0,0 +1,24 @@
+export const SearchIcon = () => {
+  return (
+    <svg width="115" height="115" viewBox="0 0 115 115">
+      <circle
+        cx="50"
+        cy="50"
+        r="34"
+        fill="none"
+        stroke="#A6A6C8"
+        strokeWidth="10"
+        strokeLinecap="round"
+      />
+      <line
+        x1="79"
+        y1="79"
+        x2="110"
+        y2="110"
+        stroke="#A6A6C8"
+        strokeWidth="10"
+        strokeLinecap="round"
+      />
+    </svg>
+  )
+}
