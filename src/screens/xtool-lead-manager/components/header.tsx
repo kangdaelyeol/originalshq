dc9bfef7f21c26c54bc6f0cf5d9c69ab016565e6
rkdeofuf <asa47972000@naver.com>
2026-07-22 12:36:28 +0900

    UI: xtool-lead header

--- src/screens/xtool-lead-manager/components/header.tsx
+++ src/screens/xtool-lead-manager/components/header.tsx
@@ -1,11 +1,23 @@
+import '@/screens/xtool-lead-manager/styles/header.scss'
+
 export const Header = () => {
   return (
     <div className="xtool-header">
-      <img
-        src="https://www.xtool.co.kr/web/upload/goodymall/kr/main/LOGO_1.png"
-        alt="xtool logo"
-      />
-      리드 매니저
+      <div className="wrapper">
+        <div className="title-box">
+          <img
+            src="https://www.xtool.co.kr/web/upload/goodymall/kr/main/LOGO_1.png"
+            alt="xtool logo"
+            className="xtool-logo"
+          />
+          <span className="title">Lead manager</span>
+        </div>
+        <img
+          src="https://framerusercontent.com/images/tuTnWS8IWm4eabExSlzNTTtkeQk.png"
+          alt="enterprise logo"
+          className="enterprise-logo"
+        />
+      </div>
     </div>
   )
 }
