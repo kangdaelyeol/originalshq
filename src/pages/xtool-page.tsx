8338d7e81dbabf856ebaaab51d8e1eb1f5deac24
rkdeofuf <asa47972000@naver.com>
2026-07-22 21:16:51 +0900

    Fix: global scss
    backgroundcolor 전역 적용

--- src/pages/xtool-page.tsx
+++ src/pages/xtool-page.tsx
@@ -1,8 +1,12 @@
 import { Footer, Header } from '@/screens/xtool-lead-manager/components'
 import { Outlet } from 'react-router-dom'
 import './xtool-global.scss'
+import { useEffect } from 'react'
 
 export const XtoolPage = () => {
+  useEffect(() => {
+    document.body.style.backgroundColor = '#292f4c'
+  }, [])
   return (
     <div className="xtool-lead-manager">
       <Header />
