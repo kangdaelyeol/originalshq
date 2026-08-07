9957a14340a761d2bb6ccb28689fae9580028ba5
rkdeofuf <asa47972000@naver.com>
2026-08-07 13:30:35 +0900

    Chore: add firebase

--- src/screens/xtool-lead-manager/components/main.tsx
+++ src/screens/xtool-lead-manager/components/main.tsx
@@ -1,5 +1,6 @@
 import '@/screens/xtool-lead-manager/styles/main.scss'
 import { SearchIcon } from '../illustration'
+import { RefObject, useEffect, useRef, useState } from 'react'
 
 const DayMill = 1000 * 60 * 60 * 24
 
@@ -35,13 +36,39 @@ const data = [
 ]
 
 export const Main = () => {
+  const [searchActive, setSearchActive] = useState(false)
+  const activeSearch = () => {
+    setSearchActive(true)
+  }
+
+  const inactiveSearch = () => {
+    setSearchActive(false)
+  }
+
+  useEffect(() => {
+    return () => {}
+  }, [])
+
   return (
     <div className="xtool-main">
       <div className="wrapper">
         <div className="nav">
-          <button className="search-btn">
-            <SearchIcon /> 검색
-          </button>
+          <SearchIcon />
+          {searchActive ? (
+            <div className="search-container">
+              <input
+                className="search-input"
+                type="text"
+                name="search"
+                id="search"
+                autoFocus
+              />
+            </div>
+          ) : (
+            <button className="search-btn" onClick={activeSearch}>
+              검색
+            </button>
+          )}
         </div>
         <div className="group"></div>
       </div>
