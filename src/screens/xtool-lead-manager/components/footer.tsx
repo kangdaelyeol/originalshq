356c350d224c4d4d053116e1b22b8db5a8cfa571
rkdeofuf <asa47972000@naver.com>
2026-08-11 12:02:48 +0900

    UI: add footer

--- src/screens/xtool-lead-manager/components/footer.tsx
+++ src/screens/xtool-lead-manager/components/footer.tsx
@@ -1,3 +1,21 @@
+import '@/screens/xtool-lead-manager/styles/footer.scss'
+
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="lead_manager_footer">
+      <div className="brand">
+        <span className="brand_name">xTool</span>
+        <span className="divider" />
+        <span className="sub_label">Lead Manager</span>
+      </div>
+
+      <div className="meta">
+        <span className="version">v1.0.0</span>
+        <span className="divider" />
+        <span className="copyright">
+          © {new Date().getFullYear()} Cyan International. All rights reserved.
+        </span>
+      </div>
+    </div>
+  )
 }
