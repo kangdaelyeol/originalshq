e82a1694cc3431cb5f20f36916a2628ec9c3f1be
rkdeofuf <asa47972000@naver.com>
2026-08-28 14:40:58 +0900

    Fix: generate eid util cond

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,4 +1,4 @@
 import { createHash } from 'crypto'
 
 export const generateExternalId = (id: string): string =>
-  `eid_${createHash('sha256').update(id).digest('hex').slice(0, 32)}`
+  createHash('sha256').update(id).digest('hex')
