cf98aadcb6a1267afe7edff82b3fddf376c1edbf
rkdeofuf <asa47972000@naver.com>
2026-09-06 16:59:59 +0900

    Refactor: distribute weekday/weekend visit experience board type

--- src/screens/xtool-lead-manager/entity/user/visit-experience.ts
+++ src/screens/xtool-lead-manager/entity/user/visit-experience.ts
@@ -19,9 +19,10 @@ type purchasePosibility =
   | '기기 사용 교육'
   | '재방문 예정'
 
-// 방문체험 보드 (평일?)
-export interface VisitExperience {
+interface VisitExperience {
   itemId: string
+  // 주말 / 평일 구분
+  isWeekendSlot: boolean
   // 방문체험 일자
   visitedAt: Timestamp
   // (예약) 체험 기기
@@ -30,8 +31,6 @@ export interface VisitExperience {
   experiencedDevice: string[]
   // 담당자
   assignedManagers: string[]
-  // 유입 경로
-  funnel?: Funnel
   // 체험 상태
   experienceState: ExperienceState
   // 구매 가능성
@@ -42,48 +41,28 @@ export interface VisitExperience {
   consultationNotes: string
   // 메모
   memo: string
+  // 소재
+  source: string
+  // 만족도 조사 발송 여부
+  satisfactionSurvey: boolean
+  contactTimeline: ContactTimeline
+  // 고객 요청사항
+  requirement: string
+  // 견적서 발행 여부
+  quotationIssued: boolean
+}
+
+// 방문체험 보드 (평일?)
+export interface WeekdayVisitExperience extends VisitExperience {
+  isWeekendSlot: false
+  // 유입 경로
+  funnel?: Funnel
   // 선샘플 진행 여부 (체크)
   isPreSampled: boolean
   // 녹음 진행 여부 (체크)
   isRecorded: boolean
-  // 견적서 발행 여부
-  quotationIssued: boolean
-  // 소재
-  source: string
-  // 고객 요청사항
-  requirement: string
-  // 만족도 조사 발송 여부
-  satisfiactionSurvey: boolean
-  contactTimeline: ContactTimeline
 }
 
-export interface WeekendVisitExperience {
-  itemId: string
-  // 방문체험 일자
-  visitedAt: Timestamp
-  // (예약) 체험 기기
-  reservedDevice: string[]
-  // (실제) 체험 기기
-  experiencedDevice: string[]
-  // 담당자
-  assignedManagers: string[]
-  // 체험 상태
-  experienceState: ExperienceState
-  // 구매 가능성
-  purchaseProbability: purchasePosibility
-  // 상담 진행 상태
-  consultationStatus: ConsultationStatus
-  // 상담 내역 (자유 텍스트)
-  consultationNotes: string
-  // 견적서 발행 여부
-  quotationIssued: boolean
-  // 만족도 조사 발송 여부
-  satisfiactionSurvey: boolean
-  // 소재
-  source: string
-  // 메모
-  memo: string
-  // 고객 요청사항
-  requirement: string
-  contactTimeline: ContactTimeline
+export interface WeekendVisitExperience extends VisitExperience {
+  isWeekendSlot: true
 }
