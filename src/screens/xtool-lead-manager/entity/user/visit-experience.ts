0e3172b2d724b4147d01ede8d351459e06b0a56d
rkdeofuf <asa47972000@naver.com>
2026-09-06 16:38:30 +0900

    Feat: add visit experience type

---
+++ src/screens/xtool-lead-manager/entity/user/visit-experience.ts
@@ -0,0 +1,57 @@
+/** 방문체험보드 Scheme */
+
+import type { ConsultationStatus, ContactTimeline, Timestamp } from './common'
+
+type Funnel = '지연유입' | '영업전환'
+
+type ExperienceState =
+  | '방문 대기'
+  | '체험 일정 연기'
+  | '체험 노쇼'
+  | '체험 완료'
+  | '체험 취소'
+
+type purchasePosibility =
+  | 'LOW' // 낮음
+  | 'MEDIUM' // 중간
+  | 'HIGH' // 높음
+  | 'VERY_HIGH' // 매우높음
+  | '기기 사용 교육'
+  | '재방문 예정'
+
+export interface VisitExperience {
+  itemId: string
+  // 방문체험 일자
+  visitedAt: Timestamp
+  // (예약) 체험 기기
+  reservedDevice: string[]
+  // (실제) 체험 기기
+  experiencedDevice: string[]
+  // 담당자
+  assignedManagers: string[]
+  // 유입 경로
+  funnel?: Funnel
+  // 체험 상태
+  experienceState: ExperienceState
+  // 구매 가능성
+  purchaseProbability: purchasePosibility
+  // 상담 진행 상태
+  consultationStatus: ConsultationStatus
+  // 상담 내역 (자유 텍스트)
+  consultationNotes: string
+  // 메모
+  memo: string
+  // 선샘플 진행 여부 (체크)
+  isPreSampled: boolean
+  // 녹음 진행 여부 (체크)
+  isRecorded: boolean
+  // 견적서 발행 여부
+  quotationIssued: boolean
+  // 소재
+  source: string
+  // 고객 요청사항
+  requirement: string
+  // 만족도 조사 발송 여부
+  satisfiactionSurvey: boolean
+  contactTimeline: ContactTimeline
+}
